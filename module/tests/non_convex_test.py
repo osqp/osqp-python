390b3226937c90ca7dc44d0c6f72db4c7da4d700
gbanjac <goran.banjac@eng.ox.ac.uk>
2018-07-18 13:54:03 +0200

    Added an additional test for the value of OSQP_NAN

--- module/tests/non_convex_test.py
+++ module/tests/non_convex_test.py
@@ -13,15 +13,14 @@ class non_convex_tests(unittest.TestCase):
     def setUp(self):
 
         # Simple QP problem
-        self.P = sparse.csc_matrix([[2., 5.], [5., 1.]])
-        self.q = np.array([3, 4])
-        self.A = sparse.csc_matrix([[-1.0, 0.], [0., -1.], [-1., 3.], [2., 5.], [3., 4]])
-        self.u = np.array([0., 0., -15, 100, 80])
-        self.l = -np.inf * np.ones(len(self.u))
-        self.opts = {'verbose': False}
+        P = sparse.csc_matrix([[2., 5.], [5., 1.]])
+        q = np.array([3, 4])
+        A = sparse.csc_matrix([[-1.0, 0.], [0., -1.], [-1., 3.], [2., 5.], [3., 4]])
+        u = np.array([0., 0., -15, 100, 80])
+        l = -np.inf * np.ones(len(u))
+        opts = {'verbose': False}
         self.model = osqp.OSQP()
-        self.model.setup(P=self.P, q=self.q, A=self.A, l=self.l, u=self.u,
-                         **self.opts)
+        self.model.setup(P=P, q=q, A=A, l=l, u=u, **opts)
 
     def test_non_convex(self):
         # Solve problem
@@ -29,5 +28,8 @@ class non_convex_tests(unittest.TestCase):
 
         # Assert close
         self.assertEqual(res.info.status_val, self.model.constant('OSQP_NON_CVX'))
-        nptest.assert_approx_equal(res.info.obj_val, self.model.constant('OSQP_NAN'))
+        nptest.assert_approx_equal(res.info.obj_val, np.nan)
+
+    def test_nan(self):
+        nptest.assert_approx_equal(self.model.constant('OSQP_NAN'), np.nan)
 
