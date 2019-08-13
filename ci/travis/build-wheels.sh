291e8163bd7568e4e6bffb70648588746c239647
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-08-13 17:54:02 -0400

    Fixed for manylinux

--- ci/travis/build-wheels.sh
+++ ci/travis/build-wheels.sh
@@ -4,17 +4,19 @@ set -e -x
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     "${PYBIN}/pip" install cmake
+    ln -s -f "${PYBIN}/cmake" /usr/bin/cmake
+    cmake --version
     "${PYBIN}/pip" install pytest
     "${PYBIN}/pip" wheel /io/ -w dist/
 done
 
 # Bundle external shared libraries into the wheels
-for whl in wheelhouse/*.whl; do
+for whl in dist/*.whl; do
     auditwheel repair "$whl" --plat $PLAT -w /io/dist/
 done
 
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install osqp --no-index -f /io/dist/
-    (cd "$HOME"; "${PYBIN}/pytest" osqp)
+    (cd "$HOME"; "${PYBIN}/python" -m pytest osqp)
 done
