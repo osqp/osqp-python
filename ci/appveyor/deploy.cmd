6a3a1743a1e5602c6a280622f821df2933d0f6f1
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-08-14 09:29:44 -0400

    ci

--- ci/appveyor/deploy.cmd
+++ ci/appveyor/deploy.cmd
@@ -13,30 +13,35 @@ set TEST_PIPI="false"
 )
 ECHO %ANACONDA_LABEL%
 
-REM Needed to enable to define OSQP_DEPLOY_DIR within the file
 
 @setlocal enabledelayedexpansion
 
 IF "%DISTRIB%"=="conda" (
 
-REM Anaconda deploy
 call anaconda -t %ANACONDA_TOKEN% upload conda-bld/**/osqp-*.tar.bz2 --user oxfordcontrol --force -l %ANACONDA_LABEL%
 if errorlevel 1 exit /b 1
 
-) ELSE IF "%DISTRIB%"=="pip" (
+) 
 
-REM pypi deploy
+
+
+
+IF "%DISTRIB%"=="pip" (
 
 IF %TEST_PYPI% == "true" (
 
-twine upload --repository testpypi --config-file ci\pypirc -p %PYPI_PASSWORD% --skip-existing dist/*
+twine upload --repository testpypi --config-file ci\pypirc -p %PYPI_PASSWORD% --skip-existing dist/osqp-*
 if errorlevel 1 exit /b 1
 
-) ELSE IF %APPVEYOR_REPO_TAG% == "true" (
+)
+ELSE
+(
+IF %APPVEYOR_REPO_TAG% == "true" (
 
-twine upload --repository pypi --config-file ci\pypirc -p %PYPI_PASSWORD% dist/*
+twine upload --repository pypi --config-file ci\pypirc -p %PYPI_PASSWORD% dist/osqp-*
 if errorlevel 1 exit /b 1
 
+)
 )
 
 )
