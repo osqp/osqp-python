cb51ebb070d6c6d9cd4a13564f5386a4ae1bba01
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-08-30 20:09:47 -0400

    Bump version: 0.6.0.dev20 → 0.6.0

--- module/interface.py
+++ module/interface.py
@@ -1,5 +1,5 @@
 """
-Python interface module for OSQP solver v0.6.0.dev20
+Python interface module for OSQP solver v0.6.0
 """
 from __future__ import print_function
 from builtins import object
--- modulepurepy/_osqp.py
+++ modulepurepy/_osqp.py
@@ -330,7 +330,7 @@ class OSQP(object):
     work    - workspace
     """
     def __init__(self):
-        self._version = "0.6.0.dev20"
+        self._version = "0.6.0"
 
     @property
     def version(self):
