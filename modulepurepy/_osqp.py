39aa8baf306b5f24a4b7d66cb0d979817a79c13f
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2019-08-31 15:00:16 -0400

    Bump version: 0.6.1.dev0 → 0.6.1

--- modulepurepy/_osqp.py
+++ modulepurepy/_osqp.py
@@ -330,7 +330,7 @@ class OSQP(object):
     work    - workspace
     """
     def __init__(self):
-        self._version = "0.6.1.dev0"
+        self._version = "0.6.1"
 
     @property
     def version(self):
