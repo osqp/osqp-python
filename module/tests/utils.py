2f228cfaf0e957ab85fd9a3489c79bf2a2ddc027
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2021-01-07 12:23:29 -0500

    started converting requirement to using ecos instead of cvxpy

--- module/tests/utils.py
+++ module/tests/utils.py
@@ -1,11 +1,38 @@
-import cvxpy as cp
-
-rel_tol = 1e-03
-abs_tol = 1e-04
-decimal_tol = 5
+#  import cvxpy as cp
+import numpy as np
+import scipy.sparse as spa
 
 
 def solve_high_accuracy(P, q, A, l, u):
+
+
+    # Form QP in the form
+    # minimize     0.5 x' * P * x + q' * x
+    # subject to   Gx <= h
+    #              Ax = b
+    # Note P and q are the same
+
+    # Divide in equalities and inequalities
+    eq_idx = np.abs(u - l) <= 1e-7
+    A_eq = A[eq_idx]
+    b_eq = u[eq_idx]
+    A_ineq = A[~eq_idx]
+    u_ineq = u[~eq_idx]
+    l_ineq = l[~eq_idx]
+
+    # Construct QP
+    P_qp = P
+    q_qp = q
+    A_qp = A_eq
+    b_qp = b_eq
+    G_qp = spa.vstack([A_ineq, -A_ineq], format='csc')
+    h_qp = spa.hstack([u_ineq, -l_ineq])
+
+
+    #
+
+
+
     x = cp.Variable(q.shape)
     if P.nnz == 0:
         quad_form = 0
