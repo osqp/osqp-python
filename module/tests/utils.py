733cb03890107cc0a063ccc18453c420ca493404
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2021-01-12 17:05:17 -0500

    Tests passing

--- module/tests/utils.py
+++ module/tests/utils.py
@@ -5,11 +5,10 @@ from cvxopt import spmatrix, matrix, solvers
 
 rel_tol = 1e-03
 abs_tol = 1e-04
-decimal_tol = 5
+decimal_tol = 4
 
 
 def solve_high_accuracy(P, q, A, l, u):
-
 
     # Form QP in the form
     # minimize     0.5 x' * P * x + q' * x
@@ -33,54 +32,28 @@ def solve_high_accuracy(P, q, A, l, u):
     G_qp = spa.vstack([A_ineq, -A_ineq], format='coo')
     h_qp = np.hstack([u_ineq, -l_ineq])
 
-    # Construct CVXOPT problem
-    P_cvxopt = spmatrix(P_qp.data.astype(float), P_qp.row, P_qp.col, P_qp.shape)
-    q_cvxopt = matrix(np.reshape(q_qp.astype(float), (-1, 1)))
-    if A_qp.nnz == 0:
-        A_cvxopt = None
-        b_cvxopt = None
-    else:
-        A_cvxopt = spmatrix(A_qp.data.astype(float), A_qp.row, A_qp.col, A_qp.shape)
-        b_cvxopt = matrix(np.reshape(b_qp, (-1, 1)))
-    if G_qp.nnz == 0:
-        G_cvxopt = None
-        h_cvxopt = None
-    else:
-        G_cvxopt = spmatrix(G_qp.data.astype(float), G_qp.row, G_qp.col, G_qp.shape)
-        h_cvxopt = matrix(np.reshape(h_qp.astype(float), (-1, 1)))
+    # Construct and solve CVXOPT problem
+    P_cvxopt = spmatrix(P_qp.data, P_qp.row, P_qp.col, P_qp.shape, tc='d')
+    q_cvxopt = matrix(q_qp, tc='d')
+    A_cvxopt = spmatrix(A_qp.data, A_qp.row, A_qp.col, A_qp.shape, tc='d')
+    b_cvxopt = matrix(b_qp, tc='d')
+    G_cvxopt = spmatrix(G_qp.data, G_qp.row, G_qp.col, G_qp.shape, tc='d')
+    h_cvxopt = matrix(h_qp, tc='d')
 
-    try:
-        solution = solvers.qp(P_cvxopt, q_cvxopt, G_cvxopt, h_cvxopt, A_cvxopt, b_cvxopt)
-    except:
-        import ipdb; ipdb.set_trace()
-    x_val = solution['x']
+    solvers.options['show_progress'] = False
+    solution = solvers.qp(P_cvxopt, q_cvxopt,
+                          G_cvxopt, h_cvxopt, A_cvxopt, b_cvxopt)
 
-    #
-    #
-    #
-    #
-    #  x = cp.Variable(q.shape)
-    #  if P.nnz == 0:
-    #      quad_form = 0
-    #  else:
-    #      quad_form = .5 * cp.quad_form(x, P)
-    #
-    #  if A.nnz == 0:
-    #      constraints = []
-    #  else:
-    #      constraints = [A @ x <= u, l <= A @ x]
-    #
-    #  problem = cp.Problem(cp.Minimize(quad_form + q @ x),
-    #                       constraints)
-    #  obj = problem.solve(solver=cp.ECOS,
-    #                      abstol=1e-10, reltol=1e-10,
-    #                      feastol=1e-10)
-    #  x_val = x.value
-    #
-    #  if A.nnz == 0:
-    #      y_val = None
-    #  else:
-    #      y_val = problem.constraints[0].dual_value - \
-    #          problem.constraints[1].dual_value
+    # Recover primal variable and solution
+    x_val = np.array(solution['x']).flatten()
+    obj = solution['primal objective']
+
+    # Recover dual variables
+    dual_eq = np.array(solution['y']).flatten()
+    dual_ineq_up = np.array(solution['z']).flatten()[:int(len(h_qp)/2)]
+    dual_ineq_low = np.array(solution['z']).flatten()[int(len(h_qp)/2):]
+    y_val = np.zeros(len(u))
+    y_val[eq_idx] = dual_eq
+    y_val[~eq_idx] = dual_ineq_up - dual_ineq_low
 
     return x_val, y_val, obj
