5445af200fc0f35ce0a2e6b866c009c5f5945e65
Bartolomeo Stellato <bartolomeo.stellato@gmail.com>
2021-01-14 12:53:27 -0500

    Switch from CVXPY + ECOS to CVXOPT as a test dependency (#51)
    * started converting requirement to using ecos instead of cvxpy
    
    * got cvxopt to solve the problem
    
    * Tests passing
    
    * Force doubles for cvxopt
    
    * Try to fix cvxopt installation python 3.9 (no wheels yet)
    
    * Try to build cvxopt mac/linux
    
    * Try new cvxopt wheels
    
    * Bump version: 0.6.2 → 0.6.2.post0
    
    * Fix release versioning
    
    * Minor edit
    
    * Another try versioning
    
    * Another try versioning
    
    * Another try versioning
    
    * Another try versioning
    
    * Another try versioning
    
    * Another try versioning
    
    * Another try versioning
    
    * Bump version: 0.6.2 → 0.6.2.post0

--- modulepurepy/_osqp.py
+++ modulepurepy/_osqp.py
@@ -330,7 +330,7 @@ class OSQP(object):
     work    - workspace
     """
     def __init__(self):
-        self._version = "0.6.2"
+        self._version = "0.6.2.post0"
 
     @property
     def version(self):
