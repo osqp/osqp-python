757cebdc1235a1d5bfb35dc773b40ae40eccf8ef
Vineet Bansal <vineetbansal@protonmail.com>
2021-12-28 16:18:16 -0500

    bumped version strings to post2; will tag this commit accordingly

--- src/osqp/interface.py
+++ src/osqp/interface.py
@@ -1,5 +1,5 @@
 """
-Python interface module for OSQP solver v0.6.2.post0
+Python interface module for OSQP solver v0.6.2.post2
 """
 from __future__ import print_function
 from builtins import object
