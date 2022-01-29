83e957cd3df4aa049c6a770843e1356846cd0422
Vineet Bansal <vineetbansal@protonmail.com>
2022-01-29 16:15:59 -0500

    changes for CI

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-import shutil as sh
+import shutil
 import sys
 from glob import glob
 from platform import system
@@ -11,23 +11,20 @@ from setuptools.command.build_ext import build_ext
 import distutils.sysconfig as sysconfig
 
 
-# Add parameters to cmake_args and define_macros
 cmake_args = ["-DUNITTESTS=OFF"]
 cmake_build_flags = []
 define_macros = []
 lib_subdir = []
 
-# Check if windows linux or mac to pass flag
 if system() == 'Windows':
     cmake_args += ['-G', 'Visual Studio 14 2015']
-    # Differentiate between 32-bit and 64-bit
     if sys.maxsize // 2 ** 32 > 0:
         cmake_args[-1] += ' Win64'
     cmake_build_flags += ['--config', 'Release']
     lib_name = 'osqp.lib'
     lib_subdir = ['Release']
 
-else:  # Linux or Mac
+else:
     cmake_args += ['-G', 'Unix Makefiles']
     lib_name = 'libosqp.a'
 
@@ -81,7 +78,7 @@ if system() == 'Windows':
 def prepare_codegen(osqp_dir, qdldl_dir):
     osqp_codegen_sources_dir = os.path.join('src', 'osqp', 'codegen', 'sources')
     if os.path.exists(osqp_codegen_sources_dir):
-        sh.rmtree(osqp_codegen_sources_dir)
+        shutil.rmtree(osqp_codegen_sources_dir)
     os.makedirs(osqp_codegen_sources_dir)
 
     # OSQP C files
@@ -97,7 +94,7 @@ def prepare_codegen(osqp_dir, qdldl_dir):
                                                 'src'))]
     osqp_codegen_sources_c_dir = os.path.join(osqp_codegen_sources_dir, 'src')
     if os.path.exists(osqp_codegen_sources_c_dir):  # Create destination directory
-        sh.rmtree(osqp_codegen_sources_c_dir)
+        shutil.rmtree(osqp_codegen_sources_c_dir)
     os.makedirs(osqp_codegen_sources_c_dir)
     for f in cfiles:  # Copy C files
         copy(f, osqp_codegen_sources_c_dir)
@@ -118,7 +115,7 @@ def prepare_codegen(osqp_dir, qdldl_dir):
                if f.endswith('.h')]
     osqp_codegen_sources_h_dir = os.path.join(osqp_codegen_sources_dir, 'include')
     if os.path.exists(osqp_codegen_sources_h_dir):  # Create destination directory
-        sh.rmtree(osqp_codegen_sources_h_dir)
+        shutil.rmtree(osqp_codegen_sources_h_dir)
     os.makedirs(osqp_codegen_sources_h_dir)
     for f in hfiles:  # Copy header files
         copy(f, osqp_codegen_sources_h_dir)
@@ -130,7 +127,7 @@ def prepare_codegen(osqp_dir, qdldl_dir):
     osqp_codegen_sources_configure_dir = os.path.join(osqp_codegen_sources_dir,
                                                       'configure')
     if os.path.exists(osqp_codegen_sources_configure_dir):
-        sh.rmtree(osqp_codegen_sources_configure_dir)
+        shutil.rmtree(osqp_codegen_sources_configure_dir)
     os.makedirs(osqp_codegen_sources_configure_dir)
     for f in configure_files:  # Copy configure files
         copy(f, osqp_codegen_sources_configure_dir)
@@ -140,7 +137,6 @@ def prepare_codegen(osqp_dir, qdldl_dir):
          osqp_codegen_sources_c_dir)
     copy(os.path.join(osqp_dir, 'include', 'CMakeLists.txt'),
          osqp_codegen_sources_h_dir)
-
 
 _osqp = Extension('osqp._osqp',
                   define_macros=define_macros,
@@ -156,10 +152,9 @@ _osqp = Extension('osqp._osqp',
                   extra_objects=[os.path.join('src', 'extension', 'src', lib_name)],
                   sources=glob(os.path.join('src', 'extension', 'src', '*.c')),
                   extra_compile_args=compile_args)
+_osqp.cmake_args = cmake_args
 
-
-prepare_codegen(osqp_dir, qdldl_dir)    # Perform at the end since extension2 is broken wrt codegen
-
+prepare_codegen(osqp_dir, qdldl_dir)
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir='', cmake_args=None):
@@ -181,7 +176,7 @@ class CmdCMakeBuild(build_ext):
 
         # Create build directory
         if os.path.exists(build_dir):
-            sh.rmtree(build_dir)
+            shutil.rmtree(build_dir)
         os.makedirs(build_dir)
         os.chdir(build_dir)
 
@@ -191,7 +186,7 @@ class CmdCMakeBuild(build_ext):
             raise RuntimeError("CMake must be installed to build OSQP")
 
         # Compile static library with CMake
-        call(['cmake'] + cmake_args + ['..', '-DUNITTESTS=OFF'])
+        call(['cmake'] + ext.cmake_args + ['..', '-DUNITTESTS=OFF'])
         call(['cmake', '--build', '.', '--target', 'osqpstatic'] +
              cmake_build_flags)
 
@@ -213,6 +208,7 @@ class CmdCMakeBuild(build_ext):
         build_args = ['--config', cfg]
 
         if system() == "Windows":
+            cmake_args += ['-G', 'Visual Studio 16 2019']
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), extdir)]
             if sys.maxsize > 2**32:
                 cmake_args += ['-A', 'x64']
@@ -222,42 +218,44 @@ class CmdCMakeBuild(build_ext):
             build_args += ['--', '-j2']
 
         if os.path.exists(self.build_temp):
-            sh.rmtree(self.build_temp)
+            shutil.rmtree(self.build_temp)
         os.makedirs(self.build_temp)
 
         _ext_name = ext.name.split('.')[-1]
         cmake_args.extend([f'-DOSQP_EXT_MODULE_NAME={_ext_name}'])
+
+        # What variables from the environment do we wish to pass on to cmake as variables?
+        cmake_env_vars = ('CMAKE_CUDA_COMPILER', 'CUDA_TOOLKIT_ROOT_DIR', 'MKL_DIR', 'MKL_ROOT')
+        for cmake_env_var in cmake_env_vars:
+            cmake_var = os.environ.get(cmake_env_var)
+            if cmake_var:
+                cmake_args.extend([f'-D{cmake_env_var}={cmake_var}'])
+
         if ext.cmake_args is not None:
             cmake_args.extend(ext.cmake_args)
-
+            
         check_call(['cmake', ext.sourcedir] + cmake_args, cwd=self.build_temp)
         check_call(['cmake', '--build', '.'] + build_args, cwd=self.build_temp)
 
 
-# Read README.rst file
-def readme():
-    with open('README.rst') as f:
-        return f.read()
+algebra = os.environ.get('OSQP_ALGEBRA', 'default')
+assert algebra in ('default', 'mkl', 'cuda'), f'Unknown algebra f{algebra}'
+if algebra == 'default':
+    package_name = 'osqpdev'
+    ext_modules = [_osqp, CMakeExtension(f'osqp.ext_default', cmake_args=['-DALGEBRA=default'])]
+else:
+    package_name = f'osqp_{algebra}'
+    ext_modules = [CMakeExtension(f'osqp_{algebra}', cmake_args=[f'-DALGEBRA={algebra}'])]
 
-
-ext_modules = [
-    _osqp,
-    CMakeExtension('osqp.ext_default', cmake_args=['-DALGEBRA=default'])
-]
-
-if bool(int(os.environ.get('OSQP_EXT_BUILD_MKL', 0))):
-    ext_modules.extend([CMakeExtension('osqp_mkl', cmake_args=['-DALGEBRA=mkl'])])
-if bool(int(os.environ.get('OSQP_EXT_BUILD_CUDA', 0))):
-    ext_modules.extend([CMakeExtension('osqp_cuda', cmake_args=['-DALGEBRA=cuda'])])
-
-setup(name='osqp1',
+setup(name=package_name,
       author='Bartolomeo Stellato, Goran Banjac',
       author_email='bartolomeo.stellato@gmail.com',
       description='OSQP: The Operator Splitting QP Solver',
-      long_description=readme(),
+      long_description=open('README.rst').read(),
       package_dir={'': 'src'},
-      include_package_data=True,  # Include package data from MANIFEST.in
+      include_package_data=True,
       install_requires=['numpy>=1.7', 'scipy>=0.13.2', 'qdldl'],
+      python_requires='>=3.7',
       license='Apache 2.0',
       url="https://osqp.org/",
       cmdclass={'build_ext': CmdCMakeBuild},
