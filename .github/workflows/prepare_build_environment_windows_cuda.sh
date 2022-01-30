3e41357ea9f3266b58b9e48296e6f28db59de0b5
Vineet Bansal <vineetbansal@protonmail.com>
2022-01-30 13:48:26 -0500

    trying cudatoolkit 11.3

--- .github/workflows/prepare_build_environment_windows_cuda.sh
+++ .github/workflows/prepare_build_environment_windows_cuda.sh
@@ -4,7 +4,10 @@ set -e
 set -x
 
 #curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_461.33_win10.exe
-curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_511.23_windows.exe
+curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.3.0/local_installers/cuda_11.3.0_465.89_win10.exe
+#curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda_11.6.0_511.23_windows.exe
+
 #./cuda.exe -s nvcc_11.2 cudart_11.2 cublas_dev_11.2 curand_dev_11.2 cusparse_dev_11.2 visual_studio_integration_11.2
-./cuda.exe -s nvcc_11.6 cudart_11.6 cublas_dev_11.6 curand_dev_11.6 cusparse_dev_11.6 visual_studio_integration_11.6
+./cuda.exe -s nvcc_11.3 cudart_11.3 cublas_dev_11.3 curand_dev_11.3 cusparse_dev_11.3 visual_studio_integration_11.3
+#./cuda.exe -s nvcc_11.6 cudart_11.6 cublas_dev_11.6 curand_dev_11.6 cusparse_dev_11.6 visual_studio_integration_11.6
 rm cuda.exe
