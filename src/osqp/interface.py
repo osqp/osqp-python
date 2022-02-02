e452494816cc26772eaa9f743015cf6cbba258b0
Vineet Bansal <vineetbansal@protonmail.com>
2022-02-02 09:30:31 -0500

    vb/ghactions fix (#66)
    * using build package to build sdist (preserves version), bumped version to .post5

--- src/osqp/interface.py
+++ src/osqp/interface.py
@@ -1,5 +1,5 @@
 """
-Python interface module for OSQP solver v0.6.2.post3
+Python interface module for OSQP solver v0.6.2.post5
 """
 from __future__ import print_function
 from builtins import object
