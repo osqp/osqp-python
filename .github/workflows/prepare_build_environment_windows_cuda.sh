a4d1f59329828ab245e6222c7bf07700b3cdfcf7
Vineet Bansal <vineetbansal@protonmail.com>
2022-02-28 12:29:56 -0500

    updated cuda toolkit on windows to 11.6

--- .github/workflows/prepare_build_environment_windows_cuda.sh
+++ .github/workflows/prepare_build_environment_windows_cuda.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.2.2/local_installers/cuda_11.2.2_461.33_win10.exe
-./cuda.exe -s nvcc_11.2 cudart_11.2 cublas_dev_11.2 curand_dev_11.2 cusparse_dev_11.2 visual_studio_integration_11.2
+curl -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/11.6.1/local_installers/cuda_11.6.1_511.65_windows.exe
+./cuda.exe -s nvcc_11.6 cudart_11.6 cublas_dev_11.6 curand_dev_11.6 cusparse_dev_11.6 visual_studio_integration_11.6
 rm cuda.exe
