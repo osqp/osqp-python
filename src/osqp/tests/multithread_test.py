7ff1cc618cfcc4ff9ddf4b20d48209bb383319ab
Vineet Bansal <vineetbansal@protonmail.com>
2022-11-22 11:29:39 -0500

    bumped up no. of problems for multithreading test

--- src/osqp/tests/multithread_test.py
+++ src/osqp/tests/multithread_test.py
@@ -15,7 +15,7 @@ class multithread_tests(unittest.TestCase):
     def test_multithread(self):
         data = []
 
-        n_rep = 50
+        n_rep = 500
 
         for i in range(n_rep):
             m = 1000
