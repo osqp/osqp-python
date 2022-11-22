c8ce6e8db1cec0152fa5bb0f3d92fcce570f49d6
Vineet Bansal <vineetbansal@protonmail.com>
2022-11-22 10:21:22 -0500

    bumped version strings to 0.6.2.post7; will tag

--- src/osqp/interface.py
+++ src/osqp/interface.py
@@ -1,5 +1,5 @@
 """
-Python interface module for OSQP solver v0.6.2.post6
+Python interface module for OSQP solver v0.6.2.post7
 """
 from __future__ import print_function
 from builtins import object
