f56cc1eb39bf422c4b8916f5711e2a05905bdefe
Marvin van Aalst <marvin.vanaalst@gmail.com>
2022-12-19 14:25:44 +0100

    fix: replace np.int alias with builtin int

--- src/osqp/utils.py
+++ src/osqp/utils.py
@@ -71,16 +71,16 @@ def prepare_data(P=None, q=None, A=None, l=None, u=None, **settings):
         # Create elements if they are not specified
         if P is None:
             P = sparse.csc_matrix((np.zeros((0,), dtype=np.double),
-                                   np.zeros((0,), dtype=np.int),
-                                   np.zeros((n+1,), dtype=np.int)),
+                                   np.zeros((0,), dtype=int),
+                                   np.zeros((n+1,), dtype=int)),
                                   shape=(n, n))
         if q is None:
             q = np.zeros(n)
 
         if A is None:
             A = sparse.csc_matrix((np.zeros((0,), dtype=np.double),
-                                   np.zeros((0,), dtype=np.int),
-                                   np.zeros((n+1,), dtype=np.int)),
+                                   np.zeros((0,), dtype=int),
+                                   np.zeros((n+1,), dtype=int)),
                                   shape=(m, n))
             l = np.zeros(A.shape[0])
             u = np.zeros(A.shape[0])
