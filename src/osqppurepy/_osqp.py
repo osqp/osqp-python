cc38014f36190633a4501117073c21f66d4cc4ae
Vineet Bansal <vineetbansal@protonmail.com>
2024-05-22 16:10:19 -0400

    bumped version to 0.6.7

--- src/osqppurepy/_osqp.py
+++ src/osqppurepy/_osqp.py
@@ -330,7 +330,7 @@ class OSQP(object):
     work    - workspace
     """
     def __init__(self):
-        self._version = "0.6.5"
+        self._version = "0.6.7"
 
     @property
     def version(self):
