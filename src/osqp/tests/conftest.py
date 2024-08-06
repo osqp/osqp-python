f0886256733f32a15b2a9a017a23ffbd9bd2b8eb
Vineet Bansal <vineetb@della8.princeton.edu>
2024-08-06 11:41:12 -0400

    renamed envvars for pytest parametrization

--- src/osqp/tests/conftest.py
+++ src/osqp/tests/conftest.py
@@ -5,8 +5,8 @@ from osqp import algebra_available
 def pytest_generate_tests(metafunc):
 
     # detect env vars to decide which algebras to include/skip
-    algebras_include = os.environ.get('OSQP_ALGEBRA_INCLUDE', 'builtin mkl-direct mkl-indirect cuda').split()
-    algebras_skip = os.environ.get('OSQP_ALGEBRA_SKIP', '').split()
+    algebras_include = os.environ.get('OSQP_TEST_ALGEBRA_INCLUDE', 'builtin mkl-direct mkl-indirect cuda').split()
+    algebras_skip = os.environ.get('OSQP_TEST_ALGEBRA_SKIP', '').split()
     algebras = [x for x in algebras_include if x not in algebras_skip]
 
     parameters = ('algebra', 'solver_type', 'atol', 'rtol', 'decimal_tol')
