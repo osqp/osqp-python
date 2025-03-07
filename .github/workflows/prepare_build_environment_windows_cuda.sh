a6c4b388cc33a9d3eb518cdb2b7cfedc75fd8161
Vineet Bansal <vineetbansal@protonmail.com>
2025-03-07 16:59:02 -0500

    updated cuda installer exe url

--- .github/workflows/prepare_build_environment_windows_cuda.sh
+++ .github/workflows/prepare_build_environment_windows_cuda.sh
@@ -3,6 +3,6 @@
 set -e
 set -x
 
-curl -s -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.5.1/local_installers/cuda_12.5.1_555.85_windows.exe
+curl -s -L -nv -o cuda.exe https://developer.download.nvidia.com/compute/cuda/12.8.1/local_installers/cuda_12.8.1_572.61_windows.exe
 ./cuda.exe -s nvcc_12.5 cudart_12.5 cublas_dev_12.5 curand_dev_12.5 cusparse_dev_12.5 thrust_12.5 visual_studio_integration_12.5
 rm cuda.exe
