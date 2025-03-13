2d25eb07cadd12b3d5587b2bccbe5af835a2132e
Vineet Bansal <vineetbansal@protonmail.com>
2025-03-13 15:28:22 -0400

    restructuring of CI steps to have as much configuration in pyproject.toml as possible; added pyodide target

--- .github/workflows/prepare_build_environment_linux_mkl.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-set -e
-set -x
-
-yum-config-manager --add-repo https://yum.repos.intel.com/oneapi
-rpm --import https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
-yum --nogpgcheck install -y intel-oneapi-mkl-devel-2023.0.0
