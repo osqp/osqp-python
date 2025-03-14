710a71629eca00c09f523b2f9d581b2e1e30936d
Vineet Bansal <vineetbansal@protonmail.com>
2025-03-14 12:09:19 -0400

    restructuring of CI steps wrt cibuildwheel (#169)
    * restructuring of CI steps to have as much configuration in pyproject.toml as possible; added pyodide target
    * using cibuildwheel.toml for cibuildwheel-specific sections

--- .github/workflows/prepare_build_environment_linux_cuda.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-set -e
-set -x
-
-yum-config-manager --add-repo https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-rhel7.repo
-yum install -y cuda-toolkit-12-4
-
-/usr/local/cuda-12.4/bin/nvcc --version
